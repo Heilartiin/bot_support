810d7d3ae6c4793f16464c4e0c8fc6cbab924507
Dmitriy Zakharov <heilartin26@gmail.com>
2021-01-27 04:36:20 +0300

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 env GOOS=linux go build -o bot_support
-ssh root@64.225.107.99 "systemctl stop bot_support.service"
-scp bot_support root@64.225.107.99:~/bot_support/bot_support
-ssh root@64.225.107.99 "systemctl start bot_support.service"
-ssh root@64.225.107.99  "systemctl status -l bot_support.service"
+ssh root@157.230.97.187 "systemctl stop bot-support.service"
+scp bot_support root@157.230.97.187:~/monitors/bot_support
+ssh root@157.230.97.187 "systemctl start bot-support.service"
+ssh root@157.230.97.187  "systemctl status -l bot-support.service"
 rm bot_support
